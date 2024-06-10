9aef6475de9ee1cf3e6a7ebfc099f22191bdac9b
Andrzej Prochyra <andrzej@prochyra.io>
2024-06-10 20:35:24 +0100

    Extract

--- Main.cs
+++ Main.cs
@@ -4,17 +4,20 @@ using System;
 public partial class Main : Node2D
 {
 	private CharacterBody2D _paddle;
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
+	{
+		InitializePaddle();
+	}
+	
+	public override void _Process(double delta)
+	{
+		
+	}
+
+	private void InitializePaddle()
 	{
 		_paddle = GetNode<CharacterBody2D>("Paddle");
 		var screenSize = GetWindow().Size;
 		_paddle.Position = new Vector2(screenSize.X / 8, screenSize.Y / 2);
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		
 	}
 }
