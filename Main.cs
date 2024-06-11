84bba9ff05ed023e97b44b9f22bc8aa3289a87ea
Petar Simonovic <petar@simonovic.com>
2024-06-11 21:16:16 +0100

    Initialise top and bottom boundaries

--- Main.cs
+++ Main.cs
@@ -8,6 +8,7 @@ public partial class Main : Node2D
 	public override void _Ready()
 	{
 		InitializePaddle();
+		InitializeBoundaries();
 	}
 
 	public override void _Process(double delta)
@@ -30,7 +31,19 @@ public partial class Main : Node2D
 	private void InitializePaddle()
 	{
 		_paddle = GetNode<Paddle>("Paddle");
-		var screenSize = GetWindow().Size;
-		_paddle.Position = new Vector2(screenSize.X / 8, screenSize.Y / 2);
+		_paddle.Position = new Vector2(ScreenSize.X / 8, ScreenSize.Y / 2);
 	}
+
+	private void InitializeBoundaries()
+	{
+		var topBoundary = GetNode<StaticBody2D>("TopBoundary");
+		var topCollisionShape = topBoundary.GetNode<CollisionShape2D>("CollisionShape2D");
+		var topWorldBoundaryShape = topCollisionShape.Shape as WorldBoundaryShape2D;
+		topWorldBoundaryShape.Normal = Vector2.Down;
+
+		var bottomBoundary = GetNode<StaticBody2D>("BottomBoundary");
+		bottomBoundary.Position = new Vector2(0, ScreenSize.Y);
+	}
+
+	private Vector2 ScreenSize => GetWindow().Size;
 }
