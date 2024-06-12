ba64326772ef2aba23f4e391b9272d69335c21c8
Petar Simonovic <petar@simonovic.com>
2024-06-12 20:44:37 +0100

    Launch ball (slowly, with gravity)

--- Main.cs
+++ Main.cs
@@ -10,6 +10,7 @@ public partial class Main : Node2D
     {
         InitializePaddle();
         InitializeBoundaries();
+		LaunchBall();
     }
 
     public override void _Process(double delta)
@@ -43,6 +44,14 @@ public partial class Main : Node2D
         var bottomBoundary = GetNode<StaticBody2D>("BottomBoundary");
         bottomBoundary.Position = new Vector2(0, ScreenSize.Y);
     }
+
+	private void LaunchBall() {
+		RigidBody2D ball = BallScene.Instantiate<RigidBody2D>();
+		ball.Position = new Vector2(ScreenSize.X / 2, ScreenSize.Y / 2);
+		ball.LinearVelocity = Vector2.Left * 200;
+		AddChild(ball);
+	}
+
 
     private Vector2 ScreenSize => GetWindow().Size;
 }
