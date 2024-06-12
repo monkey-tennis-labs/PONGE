210bf6fd26fb9dd004aafd733500f74cbfa5e7da
Andrzej Prochyra <andrzej@prochyra.io>
2024-06-12 19:48:33 +0100

    Make the world boundaries work
    We are rotating the whole upper boundary. Setting the normal to point down does not work. Paddle disappears off the screen for some reason.

--- Main.cs
+++ Main.cs
@@ -1,49 +1,46 @@
 using Godot;
-using System;
 
 public partial class Main : Node2D
 {
-	private Paddle _paddle;
+    private Paddle _paddle;
 
-	public override void _Ready()
-	{
-		InitializePaddle();
-		InitializeBoundaries();
-	}
+    public override void _Ready()
+    {
+        InitializePaddle();
+        InitializeBoundaries();
+    }
 
-	public override void _Process(double delta)
-	{
-		CheckInput(delta);
-	}
+    public override void _Process(double delta)
+    {
+        CheckInput(delta);
+    }
 
-	private void CheckInput(double delta)
-	{
-		if (Input.IsKeyPressed(Key.Up))
-		{
-		   _paddle.MoveUp((float) delta);
-		}
-		else if (Input.IsKeyPressed(Key.Down))
-		{
-			_paddle.MoveDown((float) delta);
-		}
-	}
+    private void CheckInput(double delta)
+    {
+        if (Input.IsKeyPressed(Key.Up))
+        {
+            _paddle.MoveUp((float)delta);
+        }
+        else if (Input.IsKeyPressed(Key.Down))
+        {
+            _paddle.MoveDown((float)delta);
+        }
+    }
 
-	private void InitializePaddle()
-	{
-		_paddle = GetNode<Paddle>("Paddle");
-		_paddle.Position = new Vector2(ScreenSize.X / 8, ScreenSize.Y / 2);
-	}
+    private void InitializePaddle()
+    {
+        _paddle = GetNode<Paddle>("Paddle");
+        _paddle.Position = new Vector2(ScreenSize.X / 8, ScreenSize.Y / 2);
+    }
 
-	private void InitializeBoundaries()
-	{
-		var topBoundary = GetNode<StaticBody2D>("TopBoundary");
-		var topCollisionShape = topBoundary.GetNode<CollisionShape2D>("CollisionShape2D");
-		var topWorldBoundaryShape = topCollisionShape.Shape as WorldBoundaryShape2D;
-		topWorldBoundaryShape.Normal = Vector2.Down;
+    private void InitializeBoundaries()
+    {
+        var topBoundary = GetNode<StaticBody2D>("TopBoundary");
+        topBoundary.Rotation = Mathf.Pi;
 
-		var bottomBoundary = GetNode<StaticBody2D>("BottomBoundary");
-		bottomBoundary.Position = new Vector2(0, ScreenSize.Y);
-	}
+        var bottomBoundary = GetNode<StaticBody2D>("BottomBoundary");
+        bottomBoundary.Position = new Vector2(0, ScreenSize.Y);
+    }
 
-	private Vector2 ScreenSize => GetWindow().Size;
+    private Vector2 ScreenSize => GetWindow().Size;
 }
--- Paddle.cs
+++ Paddle.cs
@@ -3,15 +3,20 @@ using System;
 
 public partial class Paddle : CharacterBody2D
 {
-	private float _speed = 1000f;
+    private float _speed = 1000f;
 
-	public void MoveUp(float delta)
-	{
-		Position = new Vector2(Position.X, Position.Y - delta * _speed);
-	}
+    public void MoveUp(float delta)
+    {
+        Velocity = new Vector2(0, -_speed);
+    }
 
-	public void MoveDown(float delta)
-	{
-		Position = new Vector2(Position.X, Position.Y + delta * _speed);
-	}
+    public void MoveDown(float delta)
+    {
+        Velocity = new Vector2(0, _speed);
+    }
+
+    public override void _Process(double delta)
+    {
+        MoveAndCollide(Velocity * (float)delta);
+    }
 }
